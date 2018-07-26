d0242d6dae2f0d4b57babd90f8af0fcbcc69235a
Dan Rapp <drapp@proofpoint.com>
2018-07-26 11:13:24 -0600

    Initial travis setup

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pytest --color=yes --cov-config coverage.cfg --cov=tv_extract --cov-fail-under=35 --cov-report term-missing --durations=10 tests
+pytest --cov --cov-report=term-missing --cov-fail-under=10 --durations=10 tests
